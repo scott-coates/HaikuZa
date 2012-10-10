4b8a1c991e938b1292a878d4b2c4ec2fd234f576
Scott Coates <scoarescoare@gmail.com>
2012-10-10 15:14:47 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustAddGirls::Application.config.secret_token = 'b79720b78ad91eea0fd578c1eb6a6bf95c254e724fb46ccf0f4ec2140e618b4420d01dfe0c0bd2fd51a491707fea4ac794979732a136d5b25b7a8c9f6ce311b7'

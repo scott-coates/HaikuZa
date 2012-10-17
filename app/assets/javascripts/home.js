d9a43b737891f4c30c152f828e6015c16821428e
Scott Coates <scoarescoare@gmail.com>
2012-10-16 17:16:04 -0700

    adding cause progress

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,3 @@
-$(function(){
+function home_init(argument) {
 	$("#home-slider").carousel(); //TODO:figure out how to only show this on home page
-});
+}
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ JustAddGirls::Application.routes.draw do
   root :to => "home#index"
   resources :users, :only => [:index, :show, :edit, :update ]
   match '/join' => 'users#join' #TODO: figure out how to automatically wire up causess conntroller
-  match '/17s' => 'causes#main'
+  match '/17s' => 'causes#main', :as=> :main_cause
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout

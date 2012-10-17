78d6875e66b208270a292e18b5c49b2559864d72
Scott Coates <scoarescoare@gmail.com>
2012-10-17 10:00:41 -0700

    redirect to referrer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     user = User.where(:provider => auth['provider'], 
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-      redirect_to root_url
+    redirect_to request.referer
   end
 
   def destroy

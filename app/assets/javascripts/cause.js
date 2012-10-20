bd053983bfd302b0eda16b9475e7b1d82f8ae3ed
Scott Coates <scoarescoare@gmail.com>
2012-10-20 14:16:55 -0700

    adding p popup for twitter

--- app/assets/javascripts/cause.js
+++ app/assets/javascripts/cause.js
@@ -5,4 +5,27 @@ function cause_init (options) {
 			return false;
 		});
 	}
+	else{
+		$("#write-haiku").click(function(){
+			var w = 450;
+			var h = 350;
+			var left = (screen.width/2)-(w/2);
+  			var top = (screen.height/2)-(h/2);
+			var text = "@Just_Add_Girls\r\nline1\r\nline2\r\nline3\r\n";
+			var hashtags = "17s";
+			window.open('http://twitter.com/share?text=' + encodeURI(text) + '&hashtags=' + encodeURI(hashtags), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
+			return false;
+		});
+
+		$("#share-cauase").click(function(){
+			var w = 450;
+			var h = 350;
+			var left = (screen.width/2)-(w/2);
+  			var top = (screen.height/2)-(h/2);
+			var text = "help me get some points yo!";
+			var url = "http://mysite.com/test";
+			window.open('http://twitter.com/share?text=' + encodeURI(text) + '&url=' + encodeURIComponent(url), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
+			return false;
+		});
+	}
 }

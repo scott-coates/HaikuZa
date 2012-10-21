30bc86c492da2d0344929a3f8741d69f5a84f3a8
Scott Coates <scoarescoare@gmail.com>
2012-10-21 11:20:37 -0700

    updated seeds and partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,7 @@
-class User
-	after_save do |the_user|
-		the_haiku = Haiku.new(tweet_id: 1, content: "hello world")
-		the_haiku.user = the_user
-		the_haiku.save
-  		the_user.add_point({point_type: :tweet, value:1, haiku: the_haiku})
-	end
-end
-
 1.upto(100) {|i|
-	the_user =  User.create! :name => "user#{'%03d' % i}",:email=> "user#{'%03d' % i}@jag.com",:age => i / 2
+	the_user = User.create! :screen_name => "user#{'%03d' % i}",:email=> "user#{'%03d' % i}@jag.com",:age => i / 2
+	the_haiku = Haiku.new(tweet_id: 1, content: "hello world")
+	the_haiku.user = the_user 
+	the_haiku.save!
+	the_user.add_point({point_type: :tweet, value:1, haiku: the_haiku})
 }
-
-# class User
-# 	after_save do |the_user|
-#   		the_user.add_point({point_type: :tweet, value:1, haiku: the_haiku})
-# 	end
-# end
-
-# 1.upto(100) {|i|
-# 	Haiku.create!(tweet_id: 1, content: "hello world") do |haiku|
-# 		haiku.user =  User.create! :name => "user#{'%03d' % i}",:email=> "user#{'%03d' % i}@jag.com",:age => i / 2 do |user|
-# 			user.add_point({point_type: :tweet, value:1, haiku: haiku})
-# 		end
-
-# 	end
-# }

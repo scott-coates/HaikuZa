fdc054204efe3ef6c585e1286ad4c08fd8d8557b
Scott Coates <scoarescoare@gmail.com>
2012-10-22 20:51:30 -0700

    updated tweet args

--- app/assets/javascripts/cause.js
+++ app/assets/javascripts/cause.js
@@ -19,9 +19,10 @@ function cause_init (options) {
 			var h = 350;
 			var left = (screen.width/2)-(w/2);
   			var top = (screen.height/2)-(h/2);
-			var text = "@my17syllables\r\n5 syllables\r\n7 syllables\r\n5 syllables\r\n";
+			var text = "5 syllables\n7 syllables\n5 syllables";
+			var via = "my17syllables";
 			var hashtags = "haiku";
-			window.open('http://twitter.com/share?text=' + encodeURI(text) + '&hashtags=' + encodeURI(hashtags), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
+			window.open('http://twitter.com/intent/tweet?text=' + encodeURI(text) + '&hashtags=' + encodeURI(hashtags) + '&via=' + encodeURI(via), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
 			return false;
 		});
 
@@ -30,9 +31,10 @@ function cause_init (options) {
 			var h = 350;
 			var left = (screen.width/2)-(w/2);
   			var top = (screen.height/2)-(h/2);
-			var text = "How would you change the world in 17 syllables? @my17syllables";
+			var text = "How would you change the world in 17 syllables?";
+			var via = "my17syllables";
 			var url = "http://justaddgirls.com/17s?referer=" + options.user.screen_name;
-			window.open('http://twitter.com/share?text=' + encodeURI(text) + '&url=' + encodeURIComponent(url), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
+			window.open('http://twitter.com/share?text=' + encodeURI(text) + '&url=' + encodeURIComponent(url)  + '&via=' + encodeURI(via), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
 			return false;
 		});
 
@@ -42,7 +44,7 @@ function cause_init (options) {
 			var left = (screen.width/2)-(w/2);
   			var top = (screen.height/2)-(h/2);
 			var text = $(this).parents('.row-fluid:first').find('p.haiku-content').html();
-			window.open('http://twitter.com/share?text=' + encodeURI(text), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
+			window.open('http://twitter.com/intent/tweet?text=' + encodeURI(text), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
 			return false;
 		});
 	}

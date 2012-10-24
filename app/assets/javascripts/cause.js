bb53f886d3afa554c81fab9353725e54e04099f3
Scott Coates <scoarescoare@gmail.com>
2012-10-24 12:20:05 -0700

    updated tweet intents

--- app/assets/javascripts/cause.js
+++ app/assets/javascripts/cause.js
@@ -15,17 +15,16 @@ function cause_init (options) {
 		});
 	}
 	else{
-		$("#write-haiku").click(function(){
-			var w = 450;
-			var h = 350;
-			var left = (screen.width/2)-(w/2);
-  			var top = (screen.height/2)-(h/2);
+		twttr.events.bind('tweet', function(event) {
+		   console.log('event')
+		});
+
+		(function(){
 			var text = "5 syllables\n7 syllables\n5 syllables";
 			var via = "just_add_girls";
 			var hashtags = "17s";
-			window.open('http://twitter.com/intent/tweet?text=' + encodeURI(text) + '&hashtags=' + encodeURI(hashtags) + '&via=' + encodeURI(via), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
-			return false;
-		});
+			$("#write-haiku").attr('href','http://twitter.com/intent/tweet?text=' + encodeURI(text) + '&hashtags=' + encodeURI(hashtags) + '&via=' + encodeURI(via));
+		})();
 
 		$("#share-cauase").click(function(){
 			var w = 450;
@@ -36,16 +35,6 @@ function cause_init (options) {
 			var via = "just_add_girls";
 			var url = "http://justaddgirls.com/17s?referer=" + options.user.screen_name;
 			window.open('http://twitter.com/share?text=' + encodeURI(text) + '&url=' + encodeURIComponent(url)  + '&via=' + encodeURI(via), 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
-			return false;
-		});
-
-		$(document).on("click",".retweet-haiku-button",function(){
-			var w = 450;
-			var h = 350;
-			var left = (screen.width/2)-(w/2);
-  			var top = (screen.height/2)-(h/2);
-			var tweet_id = $(this).data('tweet-id');
-			window.open('http://twitter.com/intent/retweet?tweet_id=' + tweet_id, 'twitterPopup', 'status = 1, width='+w+', height='+h+', top='+top+', left='+left+' resizable = 0');
 			return false;
 		});
 	}
